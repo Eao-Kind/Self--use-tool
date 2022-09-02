80d836149feecdf86fc03cdb48cc394da6777e27
Eao-kind <a2393754@gmail.com>
2022-09-02 18:32:58 +0800

    更新代码，可保留自定义配置

--- ptSiteToNas-tools/PtSiteToNas-tools.js
+++ ptSiteToNas-tools/PtSiteToNas-tools.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         PtSiteToNas-tools
 // @namespace    http://tampermonkey.net/
-// @version      1.1.3
+// @version      1.1.4
 // @description  站点cookie发送到nastools站点管理。
 // @author       Kind
 
-// @match   http://hdhome.org/index.php
-// @match   http://pt.btschool.club/index.php
+// @match   https://hdhome.org/index.php
+// @match   https://pt.btschool.club/index.php
 // @match   https://byr.pt/index.php
 // @match   https://discfan.net/index.php
 // @match   https://et8.org/index.php
@@ -35,34 +35,141 @@
 // @match   https://www.tjupt.org/index.php
 // @match   https://www.torrentleech.org/index.php
 
-// @grant        GM_xmlhttpRequest
+// @grant       GM_xmlhttpRequest
 // @grant       GM_cookie
 // ==/UserScript==
-// 可以删除所有的match，仅保留一个 @match https://*/index.php 但使用完毕后记得关闭脚本
-let nanstoolurl = "http://192.168.1.204:300"; // 请设置nas-tools的访问地址，如http://192.168.1.2:300
+// 可以删除所有的match，仅保留一个 @match https://*/index.php 但使用完毕后记得关闭脚本。默认新加站点优先级为2
+
+// 设置nas-tools的访问地址，如http://192.168.1.2:300
+let nastoolurl = "http://192.168.1.204:300";
+// 获取nas-tools的安全密钥，基础设置-安全-API密钥
+var token = "L4eYq9tfPZ3CsEaM";
+// 如果是油猴插件测试版本，请填写BETA,否则置空
+var tampermonkeyVersion = "BETA"; //
+// 下面这些不用修改
 var siteJson;
 var dorandom = "/do?random=0.19956351081249935";
+
+
 (function () {
     'use strict';
     main();
 })();
 
-async function main() {
+async function getUserSitesByApi(){
+    return new Promise((resolve, reject) => {
+        GM_xmlhttpRequest({
+            method: "POST",
+            url: nastoolurl + "/api/v1/site/sites",
+            headers: {
+                'Origin': nastoolurl,
+                "Authorization":token,
+            },
+            onload: function (response) {
+                    console.log("【Debug】nas-tools请求userSite成功,status:",JSON.parse(response.responseText).code);
+                    // console.log(response.responseText);
+                    var data = JSON.parse(response.responseText);
+                    resolve(data.data.user_sites)
+                },
+                onerror: function (response) {
+                    console.log("【Debug】nas-tools请求userSite失败");
+                }
+        })
+    })
+}
+
+async function sendSiteToNastools(data) {
+    data = "cmd=update_site&data=" + encodeURI(JSON.stringify(data));
+    GM_xmlhttpRequest({
+        method: "POST",
+        url: nastoolurl + dorandom,
+        headers : {
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            'Origin': nastoolurl,
+            'X-Requested-With': 'XMLHttpRequest',
+        },
+        data: data,
+        onload: function (response) {
+                console.log("【Debug】nas-tools请求成功,status:",JSON.parse(response.responseText).code); // code : true 为请求成功，参数不对返回非法请求
+                // console.log(response.responseText);
+            },
+            onerror: function (response) {
+                console.log("nas-tools更新站点请求失败");
+            }
+    });
+    console.log("【Debug】站点信息发送完毕");
+}
+
+async function getData() {
     console.log("【Debug】开始获取PT站点信息");
+    var data = {};
+    var ptTitle = (document.title + "").split('::')[0];
+    const pattern = /[`~!@#$^\-&*()=|{}':;'\\\[\]\.<>\/?~！@#￥……&*（）——|{}【】'；：""'。，、？\s]/g; // 去除( "等
+    ptTitle = ptTitle.replace(pattern, "").substr(0,15);
+    var ptUrl = document.URL;
+    var ptCookie = document.cookie;
+    if (tampermonkeyVersion == "BETA") {
+        GM_cookie('list', {
+            url: location.href
+        }, (cookies) => {
+            ptCookie = cookies.map(c => `${c.name}=${c.value}`).join('; ');
+            console.log('【Debug】cookie:', ptCookie);
+        });
+    }
+
+        // 默认配置
+    data.site_id = "";
+    data.site_name = ptTitle;
+    data.site_pri = 2;
+    data.site_rssurl = "";
+    data.site_signurl = ptUrl;
+    data.site_cookie = ptCookie;
+    data.site_note = "Y|1000|Y";
+    var user_sites = await getUserSitesByApi();
+    for (var i = 0, l = user_sites.length; i < l; i++) {
+        if (user_sites[i].signurl == ptUrl) {
+            var temp = user_sites[i]
+            console.log("【Debug】查询到nas-tools中该站点的信息");
+            if(temp.cookie == ptCookie){
+                console.log("【Debug】cookie未过期，退出脚本");
+                return false;
+            }
+            data.site_id = temp.id;
+            data.site_name = temp.name;
+            data.site_pri = temp.pri;
+            data.site_rssurl = temp.rssurl;
+            data.site_signurl = ptUrl;
+            data.site_cookie = ptCookie; // GM_cookie
+            data.site_note = temp.parse + '|' + temp.rule + '|' + temp.unread_msg_notify;
+            break;
+        }
+    }
+    return data;
+}
+
+async function main() {
+    var data = await getData();
+    if(data==false) return ;
+    await sendSiteToNastools(data);
+}
+
+
+async function oldGetData() {
+    // 旧nas-tools版本不支持api获取站点信息，使用该方法
+    var data = {};
     var ptCookie = "";
     GM_cookie('list', {
         url: location.href
     }, (cookies) => {
         ptCookie = cookies.map(c => `${c.name}=${c.value}`).join('; ');
-        console.log('【Debug】cookie:',ptCookie);
+        console.log('【Debug】cookie:', ptCookie);
     });
     // var ptCookie = document.cookie;
     var ptTitle = (document.title + "").split('::')[0];
     const pattern = /[`~!@#$^\-&*()=|{}':;'\\\[\]\.<>\/?~！@#￥……&*（）——|{}【】'；：""'。，、？\s]/g; // 去除( "等
     ptTitle = ptTitle.replace(pattern, "")
-    // console.log('ptTitle', ptTitle);
+        // console.log('ptTitle', ptTitle);
     var ptUrl = document.URL;
-    let data = {};
     siteJson = await getSiteJson(); // {name:site_id}
     data.site_signurl = ptUrl;
     data.site_cookie = ptCookie;
@@ -70,8 +177,8 @@ async function main() {
     if (ptTitle in siteJson) {
         console.log("【Debug】", ptTitle + " 站点已存在，site_id:", siteJson[ptTitle]);
         var info = await getSiteinfo(siteJson[ptTitle]); // 获取更详细的site信息
-        console.log("【Debug】siteInfo:",info);
-        if(ptCookie==info.cookie){
+        console.log("【Debug】siteInfo:", info);
+        if (ptCookie == info.cookie) {
             console.log("【Debug】cookie未过期，不进行更新")
             return 0;
         }
@@ -86,16 +193,16 @@ async function main() {
         data.site_rssurl = "";
         data.site_note = "Y|1000"
     }
-    await sendSiteToNastools(data);
+    return data;
 }
 
 async function getSiteJson() {
     return new Promise((resolve, reject) => {
         GM_xmlhttpRequest({
             method: "POST",
-            url: nanstoolurl + "/site",
+            url: nastoolurl + "/site",
             headers: {
-                'Origin': nanstoolurl,
+                'Origin': nastoolurl,
             },
             onload: function (response) {
                     console.log("【Debug】nas-tools 请求站点信息成功,status:",response.status);
@@ -109,28 +216,6 @@ async function getSiteJson() {
                 }
         });
     })
-}
-
-async function sendSiteToNastools(data) {
-    data = "cmd=update_site&data=" + encodeURI(JSON.stringify(data));
-    GM_xmlhttpRequest({
-        method: "POST",
-        url: nanstoolurl + dorandom,
-        headers : {
-            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-            'Origin': nanstoolurl,
-            'X-Requested-With': 'XMLHttpRequest',
-        },
-        data: data,
-        onload: function (response) {
-                console.log("【Debug】nas-tools请求成功,成功添加站点,status:",response.status); // code : true 为请求成功，参数不对返回非法请求
-                // console.log(response.responseText);
-            },
-            onerror: function (response) {
-                console.log("nas-tools更新站点请求失败");
-            }
-    });
-    console.log("【Debug】站点信息发送完毕");
 }
 
 function infoToJson(text) {
@@ -156,9 +241,9 @@ async function getSiteinfo(site_id) {
     return new Promise((resolve, reject) => {
         GM_xmlhttpRequest({
             method: "POST",
-            url: nanstoolurl + dorandom,
+            url: nastoolurl + dorandom,
             headers: {
-                'Origin': nanstoolurl,
+                'Origin': nastoolurl,
                 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
             },
             data: "cmd=get_site&data=" + encodeURI(JSON.stringify(data)),
--- ptSiteToNas-tools/README.md
+++ ptSiteToNas-tools/README.md
@@ -1,22 +1,37 @@
 github：https://github.com/Eao-Kind/Self--use-tool/blob/main/ptSiteToNas-tools/PtSiteToNas-tools.js
 
-# 使用步骤
-
 **重要说明**
 
-​	使用脚本更新站点cookie信息到nastools时，默认使用过滤规则为：日常观影，rss链接默认为空，优先级为 1。
+​	强烈建议nas-tools版本最好>=2.0.3 且 9月1号之后**更新**过容器。【**可保留rssurl、自定义的站点名称等等所有信息**】
+
+​	使用脚本更新站点cookie信息到nastools时，默认使用过滤规则为：日常观影，rss链接默认为空，优先级为 2。
+
+​	使用油猴BETA版可获取到更多站点的cookie，基础版无法获取到MT、U2的cookie(目前已知)
+
+​	注意，浏览器中站点的标签大部分需要符合以index.php为结尾，否则不能成功发送获取站点信息。如下：
+
+>  http://hdhome.org/index.php
+
+​	更多网站请查看脚本中的 @match ，目前仅有20站，大家多多反馈缺少的站点呀。
+
+# 使用步骤
 
 **油猴添加脚本**
 
 https://greasyfork.org/zh-CN/scripts/450509-ptsitetonas-tools
 
-
-
 **访问nas-tools的web**
 
-浏览器访问nas-tools，确保可以成功访问。然后修改脚本中的url地址
+浏览器访问nas-tools，确保可以成功访问。
 
-> let nanstoolurl = "http://ip:300"; // 请设置nas-tools的访问地址，如http://192.168.1.2:300
+```
+# 然后修改脚本中的url地址
+let nanstoolurl = "http://ip:300"; // 请设置nas-tools的访问地址，如http://192.168.1.2:300
+# 获取nas-tools的安全密钥，基础设置-安全-API密钥，填入token中
+var token = "L4eYq9tfPZ3CsEaM";
+// 如果油猴插件是测试版，请填写BETA,否则置空或改成其他任意字符
+var tampermonkeyVersion = "BETA"; //
+```
 
 
 
@@ -25,12 +40,6 @@ https://greasyfork.org/zh-CN/scripts/450509-ptsitetonas-tools
 ![image-20220831111322533](README.assets/image-20220831111322533.png)
 
 ![image-20220831224526263](README.assets/image-20220831224526263.png)
-
-注意，浏览器中站点的标签大部分需要符合以index.php为结尾，否则不能成功发送获取站点信息。
-
->  http://hdhome.org/index.php
-
-另外，MT的cookie脚本无法获取，需要手动添加。
 
 开启脚本之后，在浏览器中一次性打开所有站点标签，即可发送站点信息至nas-tools。可打开F12控制台查看脚本运行信息。
 
