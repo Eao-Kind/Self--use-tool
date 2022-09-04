dcb4933651956a5c76f27099a5795abb1583cf9d
Eao-Kind <53032373+Eao-Kind@users.noreply.github.com>
2022-09-04 21:40:34 +0800

    Update PtSiteToNas-tools.js
    nas-tools站点管理新增自动签到

--- ptSiteToNas-tools/PtSiteToNas-tools.js
+++ ptSiteToNas-tools/PtSiteToNas-tools.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PtSiteToNas-tools
 // @namespace    http://tampermonkey.net/
-// @version      1.1.6
+// @version      1.1.7
 // @description  站点cookie发送到nastools站点管理。
 // @author       Kind
 
@@ -17,6 +17,10 @@ let nastoolurl = "http://192.168.1.204:300";
 var token = "L4eYq9tfPZ3CsEaM";
 // 如果油猴插件是测试版(可获取更多cookie)，请填写BETA,否则置空或改成其他任意字符
 var tampermonkeyVersion = "BET_";
+// 解析rss，日常观影，不通知，每日签到 可按需修改
+var my_site_note = "Y|1000|N|Y";
+// 默认优先级为"2"（需要是字符串），可按需修改
+var my_site_pri = "2"
 // 下面这些不用修改
 var siteJson;
 var dorandom = "/do?random=0.19956351081249935";
@@ -61,7 +65,10 @@ async function sendSiteToNastools(data) {
         },
         data: data,
         onload: function (response) {
-                console.log("【Debug】nas-tools请求成功,status:",JSON.parse(response.responseText).code); // code : true 为请求成功，参数不对返回非法请求
+                console.log("【Debug】nas-tools请求成功,status:");
+                if (JSON.parse(response.responseText).code != true){ // code : true 为请求成功
+                    console.log("【Debug】添加站点失败，可能由于nas-tools更新导致参数不对");
+                }
                 // console.log(response.responseText);
             },
             onerror: function (response) {
@@ -94,11 +101,11 @@ async function getData() {
     // 默认配置
     data.site_id = "";
     data.site_name = ptTitle;
-    data.site_pri = 2;
+    data.site_pri = my_site_pri;
     data.site_rssurl = "";
     data.site_signurl = ptUrl;
     data.site_cookie = ptCookie;
-    data.site_note = "Y|1000|Y";
+    data.site_note = my_site_note;
     var user_sites = await getUserSitesByApi();
     for (var i = 0, l = user_sites.length; i < l; i++) {
         if (user_sites[i].signurl == ptUrl) {
@@ -114,7 +121,7 @@ async function getData() {
             data.site_rssurl = temp.rssurl;
             data.site_signurl = ptUrl;
             data.site_cookie = ptCookie; // GM_cookie
-            data.site_note = temp.parse + '|' + temp.rule + '|' + temp.unread_msg_notify;
+            data.site_note = temp.parse + '|' + temp.rule + '|' + temp.unread_msg_notify + '|' + temp.auto_signin;
             break;
         }
     }
